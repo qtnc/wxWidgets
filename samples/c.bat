e04d0d9072669e76b10067197166478f6551fa15
QuentinC <webmaster@quentinc.net>
2023-06-18 18:00:27 +0200

    Fix sample compilation

--- samples/c.bat
+++ samples/c.bat
@@ -1,2 +1,2 @@
 @echo off
-mingw32-make -f makefile.gcc SHELL=cmd BUILD=release SHARED=1 USE_OPENGL=0 2>&1 | unix2dos
+mingw32-make -f makefile.gcc SHELL=cmd BUILD=debug SHARED=1 USE_OPENGL=0 DEBUG_FLAG=1 DEBUG_INFO=1 VENDOR=x86
