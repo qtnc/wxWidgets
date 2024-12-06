ad64522bb92fa0bea88e7174b69c942358894467
QuentinC <webmaster@quentinc.net>
2024-12-06 20:39:13 +0100

    Batch fix

--- build/msw/c.bat
+++ build/msw/c.bat
@@ -1,2 +1,2 @@
 @echo off
-mingw32-make -f makefile.gcc SHELL=cmd BUILD=release SHARED=1 USE_OPENGL=0 VENDOR=x86 CFLAGS="-O3 -s" CXXFLAGS="-std=gnu++17 -O3 -s" LDFLAGS="-O3 -s"
+mingw32-make -f makefile.gcc SHELL=cmd BUILD=release SHARED=1 USE_OPENGL=0 VENDOR=mgw CFLAGS="-O3 -s" CXXFLAGS="-std=gnu++17 -O3 -s" LDFLAGS="-O3 -s"
--- build/msw/cdbg.bat
+++ build/msw/cdbg.bat
@@ -1,2 +1,2 @@
 @echo off
-mingw32-make -f makefile.gcc SHELL=cmd BUILD=debug SHARED=1 USE_OPENGL=0 VENDOR=x86 CXXFLAGS="-std=gnu++17" 
+mingw32-make -f makefile.gcc SHELL=cmd BUILD=debug SHARED=1 USE_OPENGL=0 VENDOR=mgw CXXFLAGS="-std=gnu++17" 
